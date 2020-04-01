3dc304d81745d17669bf0b702160640974491caa
Tom Else <thomas.else@cruk.cam.ac.uk>
2020-04-01 11:47:49 +0100

    Integrate setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name='pymcx',
       version='1.0',
-      py_modules=['pymcx'], requires=['numpy']
+      py_modules=['pymcx'],
+      requires=['numpy'],
+      description='Simple Python interface for working with MCX Monte-Carlo light simulations.',
+      author='Thomas Else',
+      author_email='thomas.else@cruk.cam.ac.uk',
+      url='https://github.com/tre26/MCXTools',
       )
