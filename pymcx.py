448f722aec0699f5a784c195d2b03a2e0245b468
Tom Else <thomas.else@cruk.cam.ac.uk>
2020-04-02 15:56:00 +0100

    Docs

--- pymcx.py
+++ pymcx.py
@@ -2,12 +2,31 @@ import numpy
 
 
 def gen_volume_file(array: numpy.ndarray, filename: str, dtype: str = "uint8"):
+    """
+
+    Parameters
+    ----------
+    array : numpy.ndarray
+    filename : str
+    dtype : str
+    """
     array = array.astype(dtype)
     array_bytes = array.tobytes("F")
     with open(filename, "wb") as f:
         f.write(array_bytes)
 
 
-def read_output_file(filename: str, mcx_settings: dict):
+def read_output_file(filename: str, mcx_settings: dict) -> numpy.ndarray:
+    """
+
+    Parameters
+    ----------
+    filename : str
+    mcx_settings : dict
+
+    Returns
+    -------
+    numpy.ndarray
+    """
     dimension = mcx_settings["Domain"]["Dim"]
-    numpy.fromfile(filename, dtype="single").reshape(dimension + (-1,), order="F")
+    return numpy.fromfile(filename, dtype="single").reshape(dimension + (-1,), order="F")
