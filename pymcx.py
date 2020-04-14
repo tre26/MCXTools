ef49f963b4073b93afda3b5d5a6d02f813bd1558
Tom Else <thomas.else@cruk.cam.ac.uk>
2020-04-14 16:24:40 +0100

    Lots of demo code

--- pymcx.py
+++ pymcx.py
@@ -1,4 +1,21 @@
 import numpy
+import json
+
+
+def modify_settings_ms(settings, ms_media, wavelength):
+    for i, __ in enumerate(settings["Domain"]["Media"]):
+        settings["Domain"]["Media"][i]["mua"] = ms_media[i](wavelength)
+    return settings
+
+
+def load_mcx_settings(filename: str) -> dict:
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def write_mcx_settings(settings: dict, filename: str):
+    with open(filename, "w") as f:
+        return json.dump(settings, f)
 
 
 def gen_volume_file(array: numpy.ndarray, filename: str, dtype: str = "uint8"):
@@ -28,5 +45,5 @@ def read_output_file(filename: str, mcx_settings: dict) -> numpy.ndarray:
     -------
     numpy.ndarray
     """
-    dimension = mcx_settings["Domain"]["Dim"]
+    dimension = tuple(mcx_settings["Domain"]["Dim"])
     return numpy.fromfile(filename, dtype="single").reshape(dimension + (-1,), order="F")
