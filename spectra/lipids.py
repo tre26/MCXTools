5af1e0cbdc5d0fbdebde192373cffff250509963
Tom Else <56722586+tre26@users.noreply.github.com>
2020-06-24 17:07:35 +0100

    Update lipids.py

--- spectra/lipids.py
+++ spectra/lipids.py
@@ -6,7 +6,7 @@ def lipid_mu_a(wavelength):
     # [1] https://omlc.org/spectra/fat/
     # Units = m^(-1)
     file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "lipids.txt")
-    t = pd.read_csv(file, skiprows=6)
+    t = pd.read_csv(file, skiprows=5, sep="\t")
     ws = t[t.columns[0]]
     mu = t[t.columns[1]] * 1000
     mu_a = interp1d(ws, mu)
